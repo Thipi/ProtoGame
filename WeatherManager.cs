a1475000d9eb33e424485fcbf16f65733fd9fc83
Thipi <37261244+Thipi@users.noreply.github.com>
2018-03-18 10:42:32 +0200

    Update and rename HillujaScript.cs to WeatherManager.cs

--- WeatherManager.cs
+++ WeatherManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HillujaScript : MonoBehaviour {
+public class WeatherManager : MonoBehaviour {
 
     public Transform rainPosition;
     public Transform snowPosition;
